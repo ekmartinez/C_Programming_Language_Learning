9545f70bb28156b0f6e4963ea7579311a4c96d50
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-10-18 12:52:54 +0000

    To add exercise_3_24

---
+++ 3 - Chapter_three/exercises/exercise_3_24.c
@@ -0,0 +1,20 @@
+/*
+3.24 (Tabular Output) Write a program that uses looping to print the following table of values.
+Use the tab escape sequence, \t, in the printf statement to separate the columns with tabs.
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	unsigned int cnt = 1;
+
+	//Print header
+	printf("N\tN^2\tN^3\tN^4\n");
+	while(cnt < 11) {
+		printf("%d\t%d\t%d\t%d\n", (cnt), (cnt * cnt),(cnt * cnt * cnt),(cnt * cnt * cnt * cnt)); 
+		cnt++;
+	}
+}
