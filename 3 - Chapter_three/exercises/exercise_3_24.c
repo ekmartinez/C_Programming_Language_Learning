1f986c8bfe9a99f1bc3b1d716e555aef8451d69d
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-10-18 12:57:14 +0000

    To add exercise_3_24

--- 3 - Chapter_three/exercises/exercise_3_24.c
+++ 3 - Chapter_three/exercises/exercise_3_24.c
@@ -8,13 +8,15 @@ Use the tab escape sequence, \t, in the printf statement to separate the columns
 
 int main(void)
 {
-
 	unsigned int cnt = 1;
 
 	//Print header
 	printf("N\tN^2\tN^3\tN^4\n");
+
 	while(cnt < 11) {
 		printf("%d\t%d\t%d\t%d\n", (cnt), (cnt * cnt),(cnt * cnt * cnt),(cnt * cnt * cnt * cnt)); 
 		cnt++;
 	}
+
+	return 0;
 }
