ae20ef3522d7739f0d1d853e96354ab950e890d9
Erick Martinez <analyst_martinez@outlook.com>
2023-11-06 07:15:33 -0400

    modified 5_10 & 5_11

--- 5 - Chapter_five/exercises/exercise_5_10_Rounding_numbers_ceil.c
+++ 5 - Chapter_five/exercises/exercise_5_10_Rounding_numbers_ceil.c
@@ -1,4 +1,5 @@
 /*
+ 
  5.10 (Rounding Numbers) An application of function ceil is rounding a value to the nearest
 integer. The statement
 
@@ -8,3 +9,59 @@ rounds the number x to the nearest integer and assigns the result to y. Write a 
 several numbers and uses the preceding statement to round each of these numbers to the nearest
 integer. For each number processed, print both the original number and the rounded number.
 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int round_up(float floating_point);
+
+int main(void) 
+{
+	float float_1 = 0.00;
+	float float_2 = 0.00;
+	float float_3 = 0.00;
+	
+	for (unsigned int counter  = 1; counter < 4; ++counter) {
+
+		float temp_float = 0.00;
+
+		printf("Enter decimal number # %d >>> ", counter);
+		scanf("%f", &temp_float);
+
+		switch(counter) {
+			case 1:
+				float_1 = temp_float;
+				break;
+			
+			case 2:
+				float_2 = temp_float;
+				break;
+
+			case 3:
+				float_3 = temp_float;
+				break;
+		}
+	}
+
+	printf("\n#\tInput\tRounded\t\tMethod\n");
+	printf("1\t%.2f\t%d\tCeil\n", float_1, round_up(float_1));
+	printf("2\t%.2f\t%d\tCeil\n", float_2, round_up(float_2));
+	printf("3\t%.2f\t%d\tCeil\n", float_3, round_up(float_3));
+	puts("");
+}
+
+int round_up(float floating_point) {
+
+	int	y = ceil(floating_point);
+	return y;
+}
+
+
+
+
+
+
+
+
+
--- 5 - Chapter_five/exercises/exercise_5_11_Rounding_numbers_floor.c
+++ 5 - Chapter_five/exercises/exercise_5_11_Rounding_numbers_floor.c
@@ -20,3 +20,51 @@ For each value read, your program should print the original value, the number ro
 nearest integer, the number rounded to the nearest tenth, the number rounded to the nearest hun-
 dredth, and the number rounded to the nearest thousandth.
 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+	double value_read_1 = 0.00;
+	double value_read_2 = 0.00;
+	double value_read_3 = 0.00;
+
+	printf("\nFloat Converter\n-------------------------------------\n");
+
+	for (unsigned int range = 1; range <= 3; ++range) { 
+		
+		double temp_var = 0.00;
+		printf("\nEnter number # %d >>> ", range);
+		
+		scanf("%lf", &temp_var);
+			
+
+		switch(range) {
+
+			case 1:
+				value_read_1 = temp_var;
+				break;
+			case 2:
+				value_read_2 = temp_var;
+				break;
+			case 3:
+				value_read_3 = temp_var;
+				break;
+		}
+	
+	} 
+	
+	// Header	
+	printf("\n#\tInput\tFloor\t10th\t100\t1000\n");
+
+
+	printf("\n1\t%.2lf\t%d\t%.2lf\t%.3lf\t%.4lf\n", value_read_1, (int)floor(value_read_1),  floor(value_read_1) / 100,  floor(value_read_1) / 1000,floor(value_read_1) / 10000) ;
+
+	printf("2\t%.2lf\t%d\t%.2lf\t%.3lf\t%.4lf\n", value_read_2,  (int)floor(value_read_2),  floor(value_read_2) / 100,  floor(value_read_2) / 1000, floor(value_read_2) / 10000) ;
+	
+	printf("3\t%.2lf\t%d\t%.2lf\t%.3lf\t%.4lf\n", value_read_3,  (int)floor(value_read_3), floor(value_read_3) / 100,  floor(value_read_3) / 1000, floor(value_read_3) / 10000) ;
+
+}
+
