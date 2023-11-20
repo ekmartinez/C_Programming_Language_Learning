73af02ccafb14fd9dbadb0d9575bed0308f56f57
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-20 20:23:52 +0000

    modified data types, conversion formula & fixed tabular output.

--- 5 - Chapter_five/exercises/exercise_5_24_currency_conversion.c
+++ 5 - Chapter_five/exercises/exercise_5_24_currency_conversion.c
@@ -9,31 +9,31 @@ an exchange rate of 1 USD = 118.87 Yen and 1 USD = 0.92 Euro.
 
 # include <stdio.h>
 
-double toYen(int dollar);
-double toEuro(int dollar);
+double toYen(double dollar);
+double toEuro(double dollar);
 
 int main(void)
 {
-	int us_dollars = 0;
+	double us_dollars = 0.00;
 
 	printf("\nEnter US Dollars >>> ");
-	scanf("%d", &us_dollars);
+	scanf("%lf", &us_dollars);
 
-	printf("\nCurrency\t\tDollars\tForeign\n");
-	printf("Yen\t%d\t%.2f\n", us_dollars, toYen(us_dollars));
-	printf("Euro\t%d\t%.2f\n",us_dollars, toEuro(us_dollars));
+	printf("%-13s%12s%12s\n","Currency", "Dollars", "Foreign");
+	printf("%-13s%12.2f%12.2f\n", "Yen", us_dollars, toYen(us_dollars));
+	printf("%-13s%12.2f%12.2f\n", "Euro", us_dollars, toEuro(us_dollars));
 }
 
-double toYen(int us_dollars) {
+double toYen(double us_dollars) {
 
-	double SPOT_RATE_US = 1.1887;
+	double SPOT_RATE_US = 118.87 / 100;
 	double conversion = (us_dollars * SPOT_RATE_US); 
 
 	return conversion; 
 }
-double toEuro(int us_dollars) {
+double toEuro(double us_dollars) {
 
-	double SPOT_RATE_US = .92;
+	double SPOT_RATE_US = 92.00 / 100;
 	double conversion = (us_dollars * SPOT_RATE_US); 
 
 	return conversion; 
