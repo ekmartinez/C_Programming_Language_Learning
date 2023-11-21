5a86a4ddf1548253aba91dde997da9c59389ad6d
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-21 12:34:16 +0000

    Completed exercise 5_25

--- 5 - Chapter_five/exercises/exercise_5_25_find_the_maximum.c
+++ 5 - Chapter_five/exercises/exercise_5_25_find_the_maximum.c
@@ -1,6 +1,5 @@
 // 5.25(Find the Maximum) Write a function that returns the largest of four floating-point numbers.
 
-
 #include <stdio.h>
 
 int find_max(int num1, int num2, int num3, int num4);
@@ -23,11 +22,9 @@ int main(void) {
 			case 1:
 				first_digit = temp_num;
 				break;
-			
 			case 2:
 				second_digit = temp_num;
 				break;
-
 			case 3:
 				third_digit = temp_num;
 				break;
@@ -36,17 +33,27 @@ int main(void) {
 				break;
 		}
 	}
-		printf("\n%d%d%d%d\n", first_digit, second_digit, third_digit, fourth_digit);
-
+		int max = find_max(first_digit, second_digit, third_digit, fourth_digit);
+		printf("\nThe maximum numer is: %d\n", max);
 
 		return 0;
 }
 
-
+// In chapter 6 we study the concept of arrays, for now we
+// work with what we have.
 int find_max(int num1, int num2, int num3, int num4) {
+	
+	int max_num = num1;
 
-	return 0;
-
-
-
+	if (num2 > max_num) {
+		max_num = num2;
+	}
+	if (num3 > max_num) {
+		max_num = num3;
+	}
+	if (num4 > max_num) {
+		max_num = num4;
+	}
+	
+	return max_num;
 }
