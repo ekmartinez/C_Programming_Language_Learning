a5056e9d3ae8e5d5662fb4d048ee3c206833d62d
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-21 13:45:07 +0000

    Completed exercise 5_15

--- 5 - Chapter_five/exercises/exercise_5_15_Hypotenuse.c
+++ 5 - Chapter_five/exercises/exercise_5_15_Hypotenuse.c
@@ -10,3 +10,44 @@ Triangle	Side1	Side2
 2			5.0		12.0
 3			8.0		15.0
 */
+
+#include <stdio.h>
+#include <math.h>
+
+double hypotenuse(double s1, double s2);
+
+int main(void) {
+
+	double side1 = 0.00;
+	double side2 = 0.00;
+	
+	puts("\nHypotenuse Calculator\n-----------------------");
+	for (unsigned int counter  = 1; counter < 3; ++counter) {
+
+		double side = 0.00;
+
+		printf("Enter side of triangle # %d >>> ", counter);
+		scanf("%lf", &side);
+
+		switch(counter) {
+			case 1:
+				side1 = side;
+				break;
+			case 2:
+				side2 = side;
+				break;
+		}
+	}
+	printf("\nHypotenus: %.2f\n\n", hypotenuse(side1, side2));
+
+	return 0;
+}
+
+double hypotenuse(double s1, double s2) {
+
+	// c^2 = sqrt(s1^2 + s2^2)
+	
+	double result = sqrt(pow(s1, 2)+ pow(s2, 2));
+
+	return result;
+}
