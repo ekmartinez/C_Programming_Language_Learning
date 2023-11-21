14f239e1b9c2d8feb802e0d6fd18425458fefa67
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-21 21:56:37 +0000

    To begin exercise 5_31

--- 5 - Chapter_five/exercises/exercise_5_31_Coin_tossing.c
+++ 5 - Chapter_five/exercises/exercise_5_31_Coin_tossing.c
@@ -6,3 +6,44 @@ tion flip that takes no arguments and returns 0 for tails and 1 for heads. [Note
 realistically simulates the coin tossing, then each side of the coin should appear approximately half
 the time for a total of approximately 50 heads and 50 tails.]
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int flip(void);
+
+int main(void)
+{
+	unsigned int heads = 0;
+	unsigned int tails = 0;
+
+	for (unsigned int counter = 1; counter < 101; counter++) {
+
+		int random_flip = flip();
+
+		switch(random_flip) {
+			case 0:
+				tails += 1;
+				break;
+			case 1:
+				heads += 1;
+				break;
+		}
+	}
+
+	printf("\nResults\n-------------\n");
+	printf("Heads: %d\n", heads);
+	printf("Tails: %d\n",tails);
+
+	return 0;
+}
+
+int flip(void) {
+	
+	//time_t t;
+	//srand((unsigned) time(&t));
+	int random_try = rand() % 2;
+	return random_try;
+}
+
