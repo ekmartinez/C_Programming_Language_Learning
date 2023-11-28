6be905562be98760e57452a5f65c511460da9f95
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-28 15:41:39 +0000

    Completed illustration exercise 10

--- 6 - Chapter_six/illustration_exercise10.c
+++ 6 - Chapter_six/illustration_exercise10.c
@@ -1,5 +1,6 @@
 // Pasing arrays and individual array elements to functions.
 
+#include <stddef.h>
 #include <stdio.h> 
 #define SIZE 5
 
@@ -21,5 +22,40 @@ int main(void)
 	puts("");
 
 
-	modifyArray(a, SIZE);
+	modifyArray(a, SIZE); // pass array a to modifyArray by reference
+	puts("The values of the modified array are:");
+
+	// output modified array
+	for (size_t i = 0; i < SIZE; ++i) {
+		printf("%3d", a[i]);
+	}
+
+	// output value of a[3]
+	printf("\n\n\nEffects of passing array element "
+			"by value:\n\nThe value of a[3] is %d\n", a[3]);
+
+	modifyElement(a[3]); // pass array element a[3] by value
+
+	// output value of a[3]
+	printf("The value of a[3] is %d\n", a[3]);
 }
+
+	// in function modifyArray, "b" points to the original array "a"
+	// in memory
+	//
+	void modifyArray(int b[], size_t size) {
+		// multiply each array element by 2
+		for (size_t j = 0; j < size; ++j) {
+
+			b[j] *= 2; // actually modifies original array
+		}
+	}
+
+	void modifyElement(int e) {
+		// multipy paramenter by 2
+		printf("Value in modifyElement is %d\n", e *= 2);
+
+	}
+
+
+
