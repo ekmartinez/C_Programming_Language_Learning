c514bb26c4c31e35b8252b4cfc826ede0e83696f
Ubuntu <ubuntu@ip-172-31-22-61.us-east-2.compute.internal>
2023-11-29 13:59:09 +0000

    Completed illustration exercise 12

--- 6 - Chapter_six/illustration_exercise12.c
+++ 6 - Chapter_six/illustration_exercise12.c
@@ -1,7 +1,5 @@
 // computing the mean, median and mode of the data
 
-
-#include <stddef.h>
 #include <stdio.h> 
 #define SIZE 99
 
@@ -9,7 +7,7 @@
 void mean(const unsigned int answer[]);
 void median(unsigned int answer[]);
 void mode(unsigned int freq[], unsigned const int answer[]);
-void bubbleSort(int a[]);
+void bubbleSort(unsigned int a[]);
 void printArray(unsigned const int a[]);
 
 int main(void)
@@ -88,5 +86,68 @@ void mode(unsigned int freq[], const unsigned int answer[]) {
 	printf("%s%11s%19s\n\n%54s\n%54s\n\n",
 			"Response", "Frequency", "Histogram",
 			"1	1	2	2", "5	0	5	0	5");
+
+	// output results
+	unsigned int largest = 0; // represents largest frequency
+	unsigned int modeValue = 0; // represents most frequent response
+
+	for (size_t rating = 1; rating <= 9; ++rating) {
+		printf("%8lu%11u		", rating, freq[rating]);
+		
+		// keep track of mode value and largest frequency value
+		if (freq[rating] > largest) {
+			largest = freq[rating];
+			modeValue = rating;
+		}
+
+		// output histogram bar represeting frequency value
+		for (unsigned int h = 1; h <= freq[rating]; ++h) {
+			printf("%s", "*");
+		}
+
+		puts("");
+
+		}
+
+		// display the mode value
+		printf("\nThe mode is the most frequent value.\n"
+				"For this run the mode %u which occurred"
+				" %u times.\n", modeValue, largest);
 }
 
+// function that sorts an array with bubble sort algorithm
+void bubbleSort(unsigned int a[]) {
+	
+	// loop to control number of passes
+	for (unsigned int pass = 1; pass < SIZE; ++pass) {
+		
+		// loop to control number of comparisons per pass
+		for (size_t j = 0; j < SIZE - 1; ++j) {
+			
+			// swap elements if out of order
+			if (a[j] > a[j + 1]) {
+				unsigned int hold = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = hold;
+			}
+
+		}
+	}
+}
+
+// output array contents (20 values per row)
+void printArray(const unsigned int a[]) {
+	
+	// output array contentss
+	for (size_t j = 0; j < SIZE; ++j) {
+		
+		if (j % 20 == 0) {
+			puts("");
+		} 
+
+		printf("%2u", a[j]);
+
+	}
+
+
+}
