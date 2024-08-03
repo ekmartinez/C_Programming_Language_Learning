2a1bb8079e2704aa690f0f3346bbd5d4994a643b
Erick Martinez <analyst_martinez@outlook.com>
2024-08-03 17:41:37 -0400

    added size_t

--- misc/format_specifiers.c
+++ misc/format_specifiers.c
@@ -19,8 +19,15 @@ int                             %d              %d
 unsigned short                  %hu             %hu
 short                           %hd             %hd
 char                            %c              %c
+size_t                          %zu
 -------------------------------------------------------
+
+size_t - The C standard says size_t represents an unsigned
+integral type and is recommended for any variable representing
+an array's size or subscripts. Type size_t is defined in header
+<stddef.h>, which is often included by other headers such as
+<stdio.h>
+
 */
-```
 
 
