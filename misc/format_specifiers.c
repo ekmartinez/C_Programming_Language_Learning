063889af2b8be2049b896291494aa3bed27ef917
Erick Martinez <analyst_martinez@outlook.com>
2024-08-04 11:47:42 -0400

    modified

--- misc/format_specifiers.c
+++ misc/format_specifiers.c
@@ -20,9 +20,9 @@ unsigned short                  %hu             %hu
 short                           %hd             %hd
 char                            %c              %c
 size_t                          %zu
+-------------------------------------------------------
 
 pointer                         %p
--------------------------------------------------------
 
 size_t - The C standard says size_t represents an unsigned
 integral type and is recommended for any variable representing
