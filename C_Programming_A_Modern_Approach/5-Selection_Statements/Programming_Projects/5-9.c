0f7d2f6c7541245db7a1fbd358485afd807d7ea0
ezzio <ezzio@gbs.com>
2024-12-09 17:32:29 -0500

    Fixed 5-9

--- C_Programming_A_Modern_Approach/5-Selection_Statements/Programming_Projects/5-9.c
+++ C_Programming_A_Modern_Approach/5-Selection_Statements/Programming_Projects/5-9.c
@@ -11,61 +11,53 @@ Enter second date (mm/dd/yy): 5/17/07
 
 int main(void) 
 {
-	int firstDay, firstMonth, firstYear;
-	int secondDay, secondMonth, secondYear;
+	int m1, d1, y1;
+	int m2, d2, y2;
 
 	printf("\n%s", "Enter first date (mm/dd/yy): ");
-	scanf("%d/%d/%d", &firstMonth, &firstDay, &firstYear);
+	scanf("%d/%d/%d", &m1, &d1, &y1);
 
 	printf("%s", "Enter second date (mm/dd/yy): ");
-	scanf("%d/%d/%d", &secondMonth, &secondDay, &secondYear);
-
-	if (firstYear < secondYear && firstYear != 0)
-		printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-			firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
+	scanf("%d/%d/%d", &m2, &d2, &y2);
 	
+	if (m1 == m2 && d1 == d2 && y1 == y2)
+		printf("%d/%d/%02d is the same as %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
 
-	else if (firstYear > secondYear)
-		printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-			secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
-	
-	else if (firstYear == secondYear) {
-		if (firstMonth < secondMonth)
-			printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-				firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
-		else if (firstMonth > secondMonth)
-			printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-				secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
-		else if (firstMonth == secondMonth) {
-			if (firstDay < secondDay)
-				printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-					firstMonth, firstDay, firstYear, secondMonth, secondDay, secondYear);
-			else if (firstDay > secondDay)
-				printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-					secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
-			else if (firstDay == secondDay)
-				printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
-					secondMonth, secondDay, secondYear, firstMonth, firstDay, firstYear);
-		}
-	}
+	if (y1 == 99 & y2 == 0)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+	else if ( y1 == 0 & y2 == 99)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+	else if (y1 < y2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+	else if (y1 > y2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+
+	if (y1 == y2 && m1 < m2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+	else if (y1 == y2 && m1 > m2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+
+	if (y1 == y2 && m1 == m2 && d1 < d2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m1, d1, y1, m2, d2, y2);
+	else if (y1 == y2 && m1 == m2 && d1 > d2)
+		printf("%d/%d/%02d is earlier than %d/%d/%02d\n", m2, d2, y2, m1, d1, y1);
+
 }
 /*
-
-
     Test Case 1:
         Input: 3/6/08 and 5/17/07
         Expected: 5/17/07 is earlier than 3/6/08
         Actual:	5/17/07 is earlier than 3/6/08
 
-    *** Test Case 2:
-        Input: 1/1/00 and 12/31/99
+    *** Test Case 2:  
+		Input: 1/1/00 and 12/31/99
         Expected: 12/31/99 is earlier than 1/1/00
-        Actual:	1/1/00 is earlier than 12/31/99
+        Actual:	12/31/99 is earlier than 1/1/00
 
     *** Test Case 3:
         Input: 7/4/21 and 7/4/21
         Expected: 7/4/21 is the same as 7/4/21
-		Actual: 7/4/21 is earlier than 7/4/21
+		Actual: 7/4/21 is the same as 7/4/21
 
     Test Case 4:
         Input: 2/29/20 and 3/1/20
@@ -95,7 +87,7 @@ int main(void)
     *** Test Case 9:
         Input: 5/5/05 and 5/5/05
         Expected: 5/5/05 is the same as 5/5/05
-        Actual: 5/5/05 is earlier than 5/5/05
+        Actual: 5/5/05 is the same as 5/5/05
 
     Test Case 10:
         Input: 10/10/10 and 9/9/10
