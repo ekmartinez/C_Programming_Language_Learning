2423e17a4d947d1641d22453c5b6f12ad3c3e9e2
ewt <ew@pi.com>
2025-09-26 13:36:19 -0400

    Completed 7_4.

--- C_Programming_A_Modern_Approach/7-Basic_Types/Programming_Projects/7_4.c
+++ C_Programming_A_Modern_Approach/7-Basic_Types/Programming_Projects/7_4.c
@@ -19,12 +19,43 @@ You may assume that any letters entered by the user are upper case.
 
 #include <stdio.h>
 
-int translate_phone(char c)  {
+char translate_phone(char c);
 
-	;;	}
 int main(void) 
 {
-	
-	;;
+	char c;
+	printf("\n%s", "Enter phone number: ");
 
+	while ((c = getchar()) != '\n') {
+		printf("%c", translate_phone(c));
+	}
+
+	puts("\n");
+
+	 return 0;
 }
+
+char translate_phone(char c)  {
+	switch (c) {
+		case 'A': case 'B': case 'C': return '2';
+		case 'D': case 'E': case 'F': return '3';
+		case 'G': case 'H': case 'I': return '4';
+		case 'J': case 'K': case 'L': return '5';
+		case 'M': case 'N': case 'O': return '6';
+		case 'P': case 'R': case 'S': return '7';
+		case 'T': case 'U': case 'V': return '8';
+		case 'W': case 'X': case 'Y': return '9';
+		default: return c;
+	}
+}
+
+/*
+Output:
+
+Enter phone number: CALLATT
+2255288
+
+Enter phone number: 1-800-COL-LECT
+1-800-265-5328
+
+*/
